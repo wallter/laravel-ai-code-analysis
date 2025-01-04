8ad171c56704717f87de02e9bd487b12a2540a03
Tyler Wall <tyler.r.wall@gmail.com>
2025-01-04 16:54:51 -0700

    chore: Add rector.php configuration file

---
+++ rector.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_110,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ]);
