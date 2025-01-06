865066e1eeeab2f6c3db0da194db1db47b391b38
Tyler Wall <tyler.r.wall@gmail.com>
2025-01-05 23:52:36 -0700

    refactor: Rename ListQueuedJobs class to QueueListCommand

--- app/Console/Commands/Queue/QueueListCommand.php
+++ app/Console/Commands/Queue/QueueListCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands\Queue;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class ListQueuedJobs extends Command
+class QueueListCommand extends Command
 {
     /**
      * The name and signature of the console command.
